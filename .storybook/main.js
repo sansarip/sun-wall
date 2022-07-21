c5524a41f464140ce64d0f0ab3c6f0b3b47805bd
Sepehr Ansaripour <pehrans@gmail.com>
2022-07-21 03:02:04 -0400

    🔧 Support absolute paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -12,5 +14,12 @@ module.exports = {
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
+  },
+  webpackFinal: async (config) => {
+    config.resolve.modules = [
+      path.resolve(__dirname, ".."),
+      "node_modules",
+    ]
+    return config;
   }
 }
