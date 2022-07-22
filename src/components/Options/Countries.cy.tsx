d6ae9249ca52e4e3241d14d31faea1e732a4a286
Sepehr Ansaripour <pehrans@gmail.com>
2022-07-22 00:15:06 -0400

    ✅ Fix test

--- src/components/Options/Countries.cy.tsx
+++ src/components/Options/Countries.cy.tsx
@@ -17,14 +17,15 @@ describe("Countries", () => {
 
   it("Given that the main button is clicked, when a value is selected, then the onSelect function should be called with the selected value", () => {
     // given
-    const value = getName("US");
-    const targetValue = getName("CH")
+    const value = "" + getName("US");
+    const targetValue = "" + getName("CH");
     const onSelectSpy = cy.spy().as("onSelectSpy");
-    mount(<Countries onSelect={onSelectSpy} value={"" + value} />);
+    mount(<Countries onSelect={onSelectSpy} value={value} />);
     cy.findByRole("button").click();
+    cy.findByPlaceholderText("Filter...").type(targetValue);
 
     // when
-    cy.findByText("" + targetValue).click();
+    cy.findByRole("menuitem", {name: targetValue}).click();
 
     // then
     cy.get("@onSelectSpy").should("have.been.calledWith", targetValue);
